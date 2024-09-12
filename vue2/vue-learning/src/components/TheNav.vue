a377e61b730db92c412936197e5450156743cbb1
Jezhora <jezhora.photo@gmail.com>
2024-09-12 12:14:01 +0300

    lesson #6 done

---
+++ vue2/vue-learning/src/components/TheNav.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
+
+const navItems = {
+  timeline: ClockIcon,
+  activities: ListBulletIcon,
+  progress: ChartBarIcon
+}
+</script>
+
+<template>
+  <nav class="sticky bottom-0 z-10 bg-white">
+    <ul class="flex justify-around items-center border-t">
+      <li v-for="(icon, page) in navItems" :key="page" class="flex-1">
+        <a :href="`#${page}`" class="flex flex-col items-center p-2 text-xs capitalize">
+          <component :is="icon" class="w-6 h-6" /> {{ page }}
+        </a>
+      </li>
+    </ul>
+  </nav>
+</template>
